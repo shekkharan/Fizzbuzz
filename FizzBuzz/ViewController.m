1f89a52b4c9493db3f535f6db70efc22f739602c
Shekhar Sasikumar <shekkharan@gmail.com>
2016-03-13 22:45:00 +0800

    made it worse but working!

--- FizzBuzz/ViewController.m
+++ FizzBuzz/ViewController.m
@@ -16,17 +16,44 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+//  for (int i = 1; i <= 100; i++) {
+//    if (!(i%3 || i%5)) {
+//      NSLog(@"Fizzbuzz - %d",i);
+//    } else if (!(i%3)) {
+//      NSLog(@"Fizz - %d",i);
+//    } else if (!(i%5)) {
+//      NSLog(@"Buzz - %d",i);
+//    } else {
+//      NSLog(@"%d",i);
+//    }
+//  }
+  
+  NSMutableArray *array = [NSMutableArray array];
+  
   for (int i = 1; i <= 100; i++) {
-    if (!(i%3 || i%5)) {
-      NSLog(@"Fizzbuzz - %d",i);
-    } else if (!(i%3)) {
-      NSLog(@"Fizz - %d",i);
-    } else if (!(i%5)) {
-      NSLog(@"Buzz - %d",i);
-    } else {
-      NSLog(@"%d",i);
+    [array addObject:[NSNumber numberWithInt:i]];
+  }
+  
+  for (int i = 2; i <= 100; i = i + 3) {
+    if (i < 100) {
+      [array replaceObjectAtIndex:i withObject:@"Fizz"];
+    }
+    
+  }
+  for (int i = 4; i <= 100; i = i + 5) {
+    if (i < 100) {
+      [array replaceObjectAtIndex:i withObject:@"Buzz"];
     }
   }
+  for (int i = 14; i <= 100; i = i + 15) {
+    if (i < 100) {
+      [array replaceObjectAtIndex:i withObject:@"FizzBuzz"];
+    }
+  }
+  for (int i = 0; i < 100; i++) {
+    NSLog(@"%@",[array objectAtIndex:i]);
+  }
+  
   // Do any additional setup after loading the view, typically from a nib.
 }
 
